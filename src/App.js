2b9d36d300a994f6cec044e76a72e056aa71811b
Pra-2514 <pranayghavghave2006@gmail.com>
2026-06-22 20:56:57 +0530

    Initial React Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import react from 'react';
+import Navbar from './components/Navbar';
+import './App.css';
+
+export default function App() {
+  
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to the Home Page</h1>
+      <p>This is a simple React application with a navbar.</p>
+    </div>
+  );
+}
